7c3fb573ddaa794bcc75f2ae56072b07af3ad171
gurgen <dosimpler.com@gmail.com>
2025-05-31 21:31:46 -0400

    some config fix.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
+      envFilePath: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env',
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
     }),
     TypeOrmModule.forRoot(),
     UsersModule,
@@ -32,7 +31,7 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService) { }
 
   configure(consumer: MiddlewareConsumer) {
     consumer
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     res.removeHeader('date');
     next();
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 }
 bootstrap();
