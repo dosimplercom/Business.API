cb1068a1e686c73db42ae923007c448136818f7b
dosimplercom <gug.chilo@gmail.com>
2025-06-04 00:53:57 -0400

    Create typeorm.config.ts

---
+++ typeorm.config.ts
@@ -0,0 +1,26 @@
+// typeorm.config.ts
+
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env (optional)
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+
+  type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity.js'], // compiled JS files in production
+      migrations: ['migrations/*.ts'],
+      ssl: {
+        rejectUnauthorized: false, // required by Azure Postgres
+        ca: process.env.PG_CA ? Buffer.from(process.env.PG_CA, 'base64').toString('ascii') : undefined
+      },
+      synchronize: false,
+      logging: false, 
+      migrationsRun: true
+});
