7ebe1cbcd6587bec290813ea7803c976eb60a090
gurgen <dosimpler.com@gmail.com>
2025-06-03 21:24:16 -0400

    some config fix

--- ormconfig.js
+++ ormconfig.js
@@ -23,7 +23,21 @@ switch (process.env.NODE_ENV) {
     });
     break;
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity.js'], // compiled JS files in production
+      ssl: {
+        rejectUnauthorized: false, // required by Azure Postgres
+      },
+      migrationsRun: true,
+    });
     break;
+
   default:
     throw new Error('unknown environment');
 }
